20a20f2af898dc2f5999297ae4ea366622066cd7
Zapeerrr <mrizkyzhafirsyah@gmail.com>
2025-10-22 20:28:24 +0700

    Update Bioskop.cpp

--- Bioskop.cpp
+++ Bioskop.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 
 int main (){
-	
-	int umur;
-	
 	cout << "+-----------------------------------------+\n";
     cout << "| PENDETEKSI KLASIFIKASI TONTONAN V.1.3.2 |\n";
     cout << "+-----------------------------------------+\n";
+	
+	int umur;
 	cout << "Masukkan umur anda\t: ";
 	cin >> umur;
 	
-	
 	if(umur < 13) {
 		cout << "\nKategori yang boleh ditonton: SU\n";
-	} else if (umur > 13 && umur <= 16) {
+	} else if (umur >= 13 && umur <= 16) {
         cout << "\nKategori yang boleh ditonton: R13\n";
-	} else if (umur > 17 && umur <= 20) {
+	} else if (umur >= 17 && umur <= 20) {
 		cout << "\nKategori yang boleh ditonton: D17\n";
 	} else {
 		cout << "\nKategori yang boleh ditonton: D21\n";
@@ -24,3 +22,4 @@ int main (){
 	
 	return 0;
 }
+
